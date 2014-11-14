3236f1c77ed7621e7533e6df8e420e6432196ea3
WRall <wm_rall@hotmail.com>
2014-11-13 20:23:58 -0800

    Basic 2 and 1 joystick drive

--- irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/BasicOneStickDriveTrainComponent.java
+++ irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/BasicOneStickDriveTrainComponent.java
@@ -1,13 +1,54 @@
 package irs1318_2014Fall_robot;
+import java.util.*;
+import edu.wpi.first.wpilibj.*;
 
-public class BasicOneStickDriveTrainComponent extends RobotComponentBase {
+public class BasicOneStickDriveTrainComponent extends RobotComponentBase
+{
+	private static final int JOYSTICK_PORT = 1;
 	
-	public void robotInit() {
-
-	}
+	private static final int LEFT_TALON_PORT = 1;
+	private static final int RIGHT_TALON_PORT = 2;
 	
-
-	public void teleopPeriodic() {
+	private static final double DEAD_ZONE = 0.2;
+	private static final double MAX_SPEED = 0.8;
+	
+	private Joystick joystick;
+	
+	private Talon leftTalon;
+	private Talon rightTalon;
+	
+	public void robotInit()
+	{
+		this.joystick = new Joystick(JOYSTICK_PORT);
 		
+		this.leftTalon = new Talon(2, LEFT_TALON_PORT);
+		this.rightTalon = new Talon(2, RIGHT_TALON_PORT);
+	}
+
+	public void teleopPeriodic()
+	{
+		double x = this.joystick.getX();
+		double y = this.joystick.getY();
+
+		double leftPower = 0.0;
+		double rightPower = 0.0;
+		
+		double radius = Math.sqrt(x*x + y*y);
+		if (radius > BasicOneStickDriveTrainComponent.DEAD_ZONE)
+		{
+			if (Math.abs(y) < Math.abs(x))
+			{
+				leftPower = BasicOneStickDriveTrainComponent.MAX_SPEED * x;
+				rightPower = -leftPower;
+			}
+			else
+			{
+				leftPower = BasicOneStickDriveTrainComponent.MAX_SPEED * y;
+				rightPower = -leftPower;
+			}
+		}
+		
+		this.leftTalon.set(leftPower);
+		this.rightTalon.set(rightPower);
 	}
 }
--- irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/BasicTwoStickDriveTrainComponent.java
+++ irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/BasicTwoStickDriveTrainComponent.java
@@ -1,13 +1,50 @@
 package irs1318_2014Fall_robot;
+import edu.wpi.first.wpilibj.*;
 
-public class BasicTwoStickDriveTrainComponent extends RobotComponentBase {
-
-	public void robotInit() {
-
-	}
+public class BasicTwoStickDriveTrainComponent extends RobotComponentBase
+{
+	private static final int LEFT_JOYSTICK_PORT = 1;
+	private static final int RIGHT_JOYSTICK_PORT = 2;
 	
-
-	public void teleopPeriodic() {
+	private static final int LEFT_TALON_PORT = 1;
+	private static final int RIGHT_TALON_PORT = 2;
+	
+	private static final double DEAD_ZONE = 0.2;
+	private static final double MAX_SPEED = 0.8;
+	
+	private Joystick leftJoystick;
+	private Joystick rightJoystick;
+	
+	private Talon leftTalon;
+	private Talon rightTalon;
+	
+	public void robotInit()
+	{
+		this.leftJoystick = new Joystick(LEFT_JOYSTICK_PORT);
+		this.rightJoystick = new Joystick(RIGHT_JOYSTICK_PORT);
 		
+		this.leftTalon = new Talon(2, LEFT_TALON_PORT);
+		this.rightTalon = new Talon(2, RIGHT_TALON_PORT);
+	}
+
+	public void teleopPeriodic()
+	{
+		double leftY = this.leftJoystick.getY();
+		double rightY = this.rightJoystick.getY();
+		
+		double leftPower = 0.0;
+		double rightPower = 0.0;
+		if (leftY > BasicTwoStickDriveTrainComponent.DEAD_ZONE || leftY < -BasicTwoStickDriveTrainComponent.DEAD_ZONE)
+		{
+			leftPower = BasicTwoStickDriveTrainComponent.MAX_SPEED * leftY;
+		}
+		
+		if (rightY > BasicTwoStickDriveTrainComponent.DEAD_ZONE || rightY < -BasicTwoStickDriveTrainComponent.DEAD_ZONE)
+		{
+			rightPower = BasicTwoStickDriveTrainComponent.MAX_SPEED * rightY;
+		}
+		
+		this.leftTalon.set(leftPower);
+		this.rightTalon.set(rightPower);
 	}
 }
