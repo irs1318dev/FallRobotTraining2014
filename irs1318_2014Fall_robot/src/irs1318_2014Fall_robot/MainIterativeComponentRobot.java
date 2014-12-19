1818fe41b5b8ceb772c8241354c5139b2094dfc7
WRall <wm_rall@hotmail.com>
2014-12-18 20:17:29 -0800

    Use collector and compressor

--- irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/MainIterativeComponentRobot.java
+++ irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/MainIterativeComponentRobot.java
@@ -5,6 +5,8 @@ public class MainIterativeComponentRobot extends IterativeComponentRobot {
 	public BotVector currentRobotComponents() {
 		BotVector v = new BotVector();
 		v.add(new AdvancedOneStickDriveTrain());
+		v.add(new Collector());
+		v.add(new CompressorController());
 		return v;
 	}
 	
