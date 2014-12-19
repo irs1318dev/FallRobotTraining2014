0064c43e7798688ec83832dbe8ecca90f95ce82f
WRall <wm_rall@hotmail.com>
2014-12-18 23:58:20 -0800

    Move the rest of the electronics constants into the appropriate file.

--- irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/CompressorController.java
+++ irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/CompressorController.java
@@ -3,18 +3,12 @@ package irs1318_2014Fall_robot;
 import edu.wpi.first.wpilibj.*;
 
 public class CompressorController extends RobotComponentBase
-{
-	private static final int SIDECAR_SLOT = SensorBase.getDefaultDigitalModule();
-	private static final int DIGITAL_IO = SensorBase.getDefaultSolenoidModule();
-	
-	private static final int PRESSURE_SWITCH_CHANNEL = 6;
-	private static final int COMPRESSOR_RELAY_CHANNEL = 1;
-	
+{	
 	private Compressor compressor;
 	
 	public void robotInit()
 	{
-		this.compressor = new Compressor(SIDECAR_SLOT, PRESSURE_SWITCH_CHANNEL, DIGITAL_IO, COMPRESSOR_RELAY_CHANNEL);
+		this.compressor = new Compressor(ElectronicsConstants.SIDECAR_SLOT, ElectronicsConstants.COMPRESSOR_PRESSURE_SWITCH_CHANNEL, ElectronicsConstants.DIGITAL_IO, ElectronicsConstants.COMPRESSOR_RELAY_CHANNEL);
 		this.compressor.start();
 	}
 }
