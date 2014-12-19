4fa5d4f417f973b2979d1b4b53efc5e81eb3a4c2
WRall <wm_rall@hotmail.com>
2014-12-18 20:09:06 -0800

    more work...

--- irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/Collector.java
+++ irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/Collector.java
@@ -4,10 +4,11 @@ import edu.wpi.first.wpilibj.*;
 
 public class Collector extends RobotComponentBase
 {
+	private static final int SIDECAR_SLOT = SensorBase.getDefaultDigitalModule();;
+	private static final int DIGITAL_IO = SensorBase.getDefaultSolenoidModule();
+	
 	private static final int JOYSTICK_PORT = 1;
-	
-	private static final int SIDECAR_SLOT = 1;
-	
+		
 	private static final int COLLECTOR_MOTOR_CHANNEL = 3;
 
 	private static final int COLLECTOR_SOLENOID_MODULE_PORT = 2;
@@ -15,10 +16,8 @@ public class Collector extends RobotComponentBase
 	private static final int COLLECTOR_EXTENDER_SOLENOID_CHANNEL = 5;
 	private static final int COLLECTOR_RETRACTOR_SOLENOID_CHANNEL = 6;
 	
-	private Joystick joystick;
-	
-	private Talon collectorTalon;
-	
+	private Joystick joystick;	
+	private Talon collectorTalon;	
 	private DoubleSolenoid collectorSolenoid;
 	
 	public void robotInit()
@@ -33,6 +32,14 @@ public class Collector extends RobotComponentBase
 	public void teleopPeriodic()
 	{
 		// get the X and Y values from the joystick
-		// double x = this.joystick.getRawButton();
+		boolean in = this.joystick.getRawButton(1);
+		boolean out = this.joystick.getRawButton(2);
+		
+		if (in)
+		{
+		}
+		else if (out)
+		{
+		}
 	}
 }
--- irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/CompressorController.java
+++ irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/CompressorController.java
@@ -6,15 +6,17 @@ public class CompressorController extends RobotComponentBase
 {
 	private static final int JOYSTICK_PORT = 1;
 	
-	private static final int SIDECAR_SLOT = 1;
+	private static final int SIDECAR_SLOT = SensorBase.getDefaultDigitalModule();
+	private static final int DIGITAL_IO = SensorBase.getDefaultSolenoidModule();
+	
+	private static final int PRESSURE_SWITCH_CHANNEL = 6;
+	private static final int COMPRESSOR_RELAY_CHANNEL = 1;
 	
 	private Compressor compressor;
 	
 	public void robotInit()
 	{
-	}
-	
-	public void teleopPeriodic()
-	{
+		this.compressor = new Compressor(SIDECAR_SLOT, PRESSURE_SWITCH_CHANNEL, DIGITAL_IO, COMPRESSOR_RELAY_CHANNEL);
+		this.compressor.start();
 	}
 }
