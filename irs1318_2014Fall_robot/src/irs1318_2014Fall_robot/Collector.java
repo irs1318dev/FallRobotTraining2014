6c34855404c6fb53f44992a4d1d9cf58f7172743
WRall <wm_rall@hotmail.com>
2014-12-18 19:59:53 -0800

    Add collector code...

---
+++ irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/Collector.java
@@ -0,0 +1,38 @@
+package irs1318_2014Fall_robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class Collector extends RobotComponentBase
+{
+	private static final int JOYSTICK_PORT = 1;
+	
+	private static final int SIDECAR_SLOT = 1;
+	
+	private static final int COLLECTOR_MOTOR_CHANNEL = 3;
+
+	private static final int COLLECTOR_SOLENOID_MODULE_PORT = 2;
+
+	private static final int COLLECTOR_EXTENDER_SOLENOID_CHANNEL = 5;
+	private static final int COLLECTOR_RETRACTOR_SOLENOID_CHANNEL = 6;
+	
+	private Joystick joystick;
+	
+	private Talon collectorTalon;
+	
+	private DoubleSolenoid collectorSolenoid;
+	
+	public void robotInit()
+	{
+		this.joystick = new Joystick(JOYSTICK_PORT);
+		
+		this.collectorSolenoid = new DoubleSolenoid(COLLECTOR_SOLENOID_MODULE_PORT, COLLECTOR_EXTENDER_SOLENOID_CHANNEL, COLLECTOR_RETRACTOR_SOLENOID_CHANNEL);
+	
+		this.collectorTalon = new Talon(SIDECAR_SLOT, COLLECTOR_MOTOR_CHANNEL);
+	}
+	
+	public void teleopPeriodic()
+	{
+		// get the X and Y values from the joystick
+		// double x = this.joystick.getRawButton();
+	}
+}
---
+++ irs1318_2014Fall_robot/src/irs1318_2014Fall_robot/CompressorController.java
@@ -0,0 +1,20 @@
+package irs1318_2014Fall_robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class CompressorController extends RobotComponentBase
+{
+	private static final int JOYSTICK_PORT = 1;
+	
+	private static final int SIDECAR_SLOT = 1;
+	
+	private Compressor compressor;
+	
+	public void robotInit()
+	{
+	}
+	
+	public void teleopPeriodic()
+	{
+	}
+}
